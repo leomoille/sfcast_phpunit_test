bd4feb89a443d74f2ff7d5edb7ffc18503493765
Léo Moille <leomoille18@gmail.com>
2023-05-12 16:13:02 +0200

    9 - GitHub Service: Implementation

--- src/Service/GithubService.php
+++ src/Service/GithubService.php
@@ -3,12 +3,53 @@
 namespace App\Service;
 
 use App\Enum\healthStatus;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 class GithubService
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function getHealthReport(string $dinosaurName): healthStatus
     {
-        return healthStatus::HEALTHY;
+        $health = healthStatus::HEALTHY;
+        $client = HttpClient::create();
+
+        $response = $client->request(
+            method: 'GET',
+            url: 'https://api.github.com/repos/SymfonyCasts/dino-park/issues'
+        );
+
+        $this->logger->info('Request Dino Issues', [
+            'dino'           => $dinosaurName,
+            'responseStatus' => $response->getStatusCode(),
+        ]);
+
+        foreach ($response->toArray() as $issue) {
+            if (str_contains($issue['title'], $dinosaurName)) {
+                $health = $this->getDinoStatusFromLabels($issue['labels']);
+            }
+        }
+
+        return $health;
+    }
+
+    private function getDinoStatusFromLabels(array $labels): HealthStatus
+    {
+        $status = null;
+        foreach ($labels as $label) {
+            $label = $label['name'];
+            // We only care about "Status" labels
+            if (!str_starts_with($label, 'Status:')) {
+                continue;
+            }
+            // Remove the "Status:" and whitespace from the label
+            $status = trim(substr($label, strlen('Status:')));
+        }
+
+        return HealthStatus::tryFrom($status);
     }
 
 }
